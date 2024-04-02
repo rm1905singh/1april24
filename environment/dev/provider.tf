304cf54d82b71a8460170a43936c76612ca9c727
rm1905singh <158067269+rm1905singh@users.noreply.github.com>
2024-04-02 21:57:27 +0530

    added backend codes

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,10 +5,20 @@ terraform {
       version = "3.97.1"
     }
   }
+
 }
 
 provider "azurerm" {
   features {
 
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rm-rgs"
+    storage_account_name = "rmca11"
+    container_name       = "rmca12"
+    key                  = "devtf.tfstate"
   }
 }
