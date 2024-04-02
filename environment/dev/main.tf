d28208c3a46d421553f593a18dbc0090c2367e6a
rm1905singh <158067269+rm1905singh@users.noreply.github.com>
2024-04-02 22:20:51 +0530

    new add

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -9,40 +9,41 @@ module "mvnet" {
   vnts       = var.vnts
 
 }
-module "msubnets" {
-  depends_on = [module.mvnet]
-  source     = "../../modules/3subnets"
-  subnets    = var.subnets
 
-}
-module "mpips" {
-  depends_on = [var.rgs]
-  source     = "../../modules/4pips"
-  pips       = var.pips
+# module "msubnets" {
+#   depends_on = [module.mvnet]
+#   source     = "../../modules/3subnets"
+#   subnets    = var.subnets
 
-}
-module "mnics" {
-  depends_on = [module.mpips]
-  source     = "../../modules/5nics"
-  nics       = var.nics
+# }
+# module "mpips" {
+#   depends_on = [var.rgs]
+#   source     = "../../modules/4pips"
+#   pips       = var.pips
 
-}
-module "mnsg" {
-  depends_on    = [module.mrg]
-  source        = "../../modules/6nsg"
-  nsgs          = var.nsg
-  security_rule = var.security_rule
+# }
+# module "mnics" {
+#   depends_on = [module.mpips]
+#   source     = "../../modules/5nics"
+#   nics       = var.nics
 
-}
-module "mnsgass" {
-  depends_on = [module.mnsg]
-  source     = "../../modules/7nsgass"
-  nsgass     = var.nsgass
+# }
+# module "mnsg" {
+#   depends_on    = [module.mrg]
+#   source        = "../../modules/6nsg"
+#   nsgs          = var.nsg
+#   security_rule = var.security_rule
 
-}
-module "mvm" {
-  depends_on = [var.nics]
-  source     = "../../modules/8vms"
-  vms        = var.vms
+# }
+# module "mnsgass" {
+#   depends_on = [module.mnsg]
+#   source     = "../../modules/7nsgass"
+#   nsgass     = var.nsgass
 
-}
+# }
+# module "mvm" {
+#   depends_on = [var.nics]
+#   source     = "../../modules/8vms"
+#   vms        = var.vms
+
+# }
